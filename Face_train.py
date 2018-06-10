5319749db8ca87fec8fc0cf30986792b11bed09b
Mo <medsriha@live.fr>
2018-06-10 11:26:03 -0400

    Fare Train Script

---
+++ Face_train.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+#Directory path name where the face images are stored.
+path = 'images'
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+#Haar cascade file
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    faceSamples=[]
+    ids = []
+    for imagePath in imagePaths:
+        # convert it to grayscale
+        PIL_img = Image.open(imagePath).convert('L')
+        img_numpy = np.array(PIL_img,'uint8')
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+    return faceSamples,ids
+print ("\n[INFO] Training faces...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+# Save the model into the current directory.
+recognizer.write('trainer.yml')
+print("\n[INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
