45302f767decb3284fe2f3bba54162699dd81785
Mo <medsriha@gmail.com>
2023-12-26 18:47:12 -0600

    Update face_taker.py

--- face_taker.py
+++ face_taker.py
@@ -2,41 +2,75 @@ import numpy as np
 import cv2
 import os
 
-# Check if folder exists
-if not os.path.exists('images'):
-    os.makedirs('images')
+if __name__ == "__main__":
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cam = cv2.VideoCapture(0)
-cam.set(3,640)
-cam.set(4,480)
-count = 0
-
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# For each person, enter one unique numeric face id
-face_id = input('\n enter user id (MUST be an integer) and press <return> -->  ')
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-
-
-while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-        count += 1
-        # Save the captured image into the images directory
-        cv2.imwrite("./images/Users." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
-        cv2.imshow('image', img)
-    # Press Escape to end the program.
-    k = cv2.waitKey(100) & 0xff
-    if k < 30:
-        break
-    # Take 30 face samples and stop video. You may increase or decrease the number of
-    # images. The more the better while training the model.
-    elif count >= 30:
-         break
-
-print("\n [INFO] Exiting Program.")
-cam.release()
-cv2.destroyAllWindows()
+    
+    def create_directory(directory):
+        """
+        Create a directory if it doesn't exist.
+    
+        Parameters:
+            directory (str): The path of the directory to be created.
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    
+    # Create 'images' directory if it doesn't exist
+    create_directory('images')
+    
+    # Load the pre-trained face cascade classifier
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    
+    # Open a connection to the default camera (camera index 0)
+    cam = cv2.VideoCapture(0)
+    
+    # Set camera dimensions
+    cam.set(3, 640)
+    cam.set(4, 480)
+    
+    # Initialize face capture variables
+    count = 0
+    face_id = input('\nEnter user id (MUST be an integer) and press <return> -->  ')
+    print("\n[INFO] Initializing face capture. Look at the camera and wait...")
+    
+    while True:
+        # Read a frame from the camera
+        ret, img = cam.read()
+    
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+        # Detect faces in the frame
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    
+        # Process each detected face
+        for (x, y, w, h) in faces:
+            # Draw a rectangle around the detected face
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+            # Increment the count for naming the saved images
+            count += 1
+    
+            # Save the captured image into the 'images' directory
+            cv2.imwrite(f"./images/Users.{face_id}.{count}.jpg", gray[y:y+h, x:x+w])
+    
+            # Display the image with rectangles around faces
+            cv2.imshow('image', img)
+    
+        # Press Escape to end the program
+        k = cv2.waitKey(100) & 0xff
+        if k < 30:
+            break
+    
+        # Take 30 face samples and stop video. You may increase or decrease the number of
+        # images. The more, the better while training the model.
+        elif count >= 30:
+            break
+    
+    print("\n[INFO] Exiting Program.")
+    
+    # Release the camera
+    cam.release()
+    
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
