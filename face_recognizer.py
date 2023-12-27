dddd9ca48197b639861821ac0833ac04a30c2e52
Mo <medsriha@gmail.com>
2023-12-26 18:50:28 -0600

    Update face_recognizer.py

--- face_recognizer.py
+++ face_recognizer.py
@@ -2,56 +2,78 @@ import cv2
 import numpy as np
 import os
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer.yml')
-
-face_cascade_Path = "haarcascade_frontalface_default.xml"
-
-
-faceCascade = cv2.CascadeClassifier(face_cascade_Path)
-
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-id = 0
-# names related to ids: The names associated to the ids: 1 for Mohamed, 2 for Jack, etc...
-names = ['None'] # add a name into this list
-#Video Capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640)
-cam.set(4, 480)
-# Min Height and Width for the  window size to be recognized as a face
-minW = 0.1 * cam.get(3)
-minH = 0.1 * cam.get(4)
-while True:
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(int(minW), int(minH)),
-    )
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-        if (confidence < 100):
-            id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
-        else:
-            # Unknown Face
-            id = "Who are you ?"
-            confidence = "  {0}%".format(round(100 - confidence))
-
-        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
-
-    cv2.imshow('camera', img)
-    # Escape to exit the webcam / program
-    k = cv2.waitKey(10) & 0xff
-    if k == 27:
-        break
-print("\n [INFO] Exiting Program.")
-cam.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    
+    # Create LBPH Face Recognizer
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    # Load the trained model
+    recognizer.read('trainer.yml')
+    
+    # Path to the Haar cascade file for face detection
+    face_cascade_Path = "haarcascade_frontalface_default.xml"
+    
+    # Create a face cascade classifier
+    faceCascade = cv2.CascadeClassifier(face_cascade_Path)
+    
+    # Font for displaying text on the image
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    # Initialize user IDs and associated names
+    id = 0
+    names = ['None']  # Add names associated with user IDs
+    
+    # Video Capture from the default camera (camera index 0)
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)  # Set width
+    cam.set(4, 480)  # Set height
+    
+    # Minimum width and height for the window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+    
+    while True:
+        # Read a frame from the camera
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+        # Detect faces in the frame
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
+        )
+    
+        for (x, y, w, h) in faces:
+            # Draw a rectangle around the detected face
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+            # Recognize the face using the trained model
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+    
+            if confidence < 100:
+                # Recognized face
+                id = names[id]
+                confidence = "  {0}%".format(round(100 - confidence))
+            else:
+                # Unknown face
+                id = "Who are you ?"
+                confidence = "  {0}%".format(round(100 - confidence))
+    
+            # Display the recognized name and confidence level on the image
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+    
+        # Display the image with rectangles around faces
+        cv2.imshow('camera', img)
+    
+        # Press Escape to exit the webcam / program
+        k = cv2.waitKey(10) & 0xff
+        if k == 27:
+            break
+    
+    print("\n [INFO] Exiting Program.")
+    # Release the camera
+    cam.release()
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
